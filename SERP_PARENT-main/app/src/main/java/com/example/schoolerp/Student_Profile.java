e3aa8d01afbd5004a07313e2a58164cb7f8885d9
Dharani <dharani.h26@gmail.com>
2024-07-01 11:37:47 +0530

    Check Document Fragment

--- SERP_PARENT-main/app/src/main/java/com/example/schoolerp/Student_Profile.java
+++ SERP_PARENT-main/app/src/main/java/com/example/schoolerp/Student_Profile.java
@@ -10,11 +10,13 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
 
 public class Student_Profile extends Fragment {
 
@@ -62,6 +64,15 @@ public class Student_Profile extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.profile_student, container, false);
+        View view = inflater.inflate(R.layout.profile_student, container, false);
+
+        // Find the TextView by its ID
+        TextView viewMore = view.findViewById(R.id.viewMore);
+
+        // Set an OnClickListener for the TextView
+        viewMore.setOnClickListener(v -> NavHostFragment.findNavController(Student_Profile.this)
+                .navigate(R.id.fragment_more_student_details));
+
+        return view;
     }
 }
