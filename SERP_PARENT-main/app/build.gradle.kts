9e4c1fb447b53683d990a0f922b0f47c369eb175
Dharani <dharani.h26@gmail.com>
2024-07-02 16:55:16 +0530

    Check

--- SERP_PARENT-main/app/build.gradle.kts
+++ SERP_PARENT-main/app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     alias(libs.plugins.androidApplication)
     id("com.google.gms.google-services")
-    alias(libs.plugins.googleAndroidLibrariesMapsplatformSecretsGradlePlugin)
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+
 }
 
 android {
