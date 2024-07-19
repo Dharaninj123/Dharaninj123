00714fa262ff9584bd35e57d989389e567a6a18f
Dharani <dharani.h26@gmail.com>
2024-07-19 16:25:07 +0530

    Check

---
+++ SERP_PARENT-main/app/src/main/java/com/example/schoolerp/apiservices/modelclass/AadhaarCardResponse.java
@@ -0,0 +1,14 @@
+package com.example.schoolerp.apiservices.modelclass;
+
+public class AadhaarCardResponse {
+
+    private String aadhar_document_url;
+
+    public String getAadhar_document_url(){
+        return aadhar_document_url;
+    }
+
+    public void setAadhar_document_url(String aadhar_document_url) {
+        this.aadhar_document_url = aadhar_document_url;
+    }
+}
